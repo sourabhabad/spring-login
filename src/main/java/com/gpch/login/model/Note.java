a28a552445a9d0009bda250e5132014031c854e4
Sourabh Kumar <sourabh.k@ET-C02VN10HG8WN.local>
2019-10-22 15:01:15 +0530

    added Note

---
+++ src/main/java/com/gpch/login/model/Note.java
@@ -0,0 +1,31 @@
+package com.gpch.login.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
+public class Note implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    private String email;
+
+}
---
+++ src/main/java/com/gpch/login/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.gpch.login.repository;
+
+import com.gpch.login.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
